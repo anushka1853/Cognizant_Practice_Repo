82917b7de73cfc77486456756adbc56f6188ae37
Anushka <128998121+anushka1853@users.noreply.github.com>
2025-07-15 02:15:37 +0530

    Create Create a Spring Web Project using Maven.java

---
+++ Week 4 Spring REST using Spring Boot 3/Spring REST handson/Create a Spring Web Project using Maven.java
@@ -0,0 +1,39 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Build & run (from project root with pom.xml):
+ *
+ *   mvn spring-boot:run
+ *
+ * Then browse to:
+ *   http://localhost:8080/hello
+ */
+@SpringBootApplication
+@RestController                    // the same file doubles as controller
+public class SpringLearnApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+    public static void main(String[] args) {
+        log.debug("START main()");
+        SpringApplication.run(SpringLearnApplication.class, args);
+        log.debug("END main()");
+    } 
+  /* REST ENDPOINT: Hello World */
+
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        log.debug("START sayHello()");
+        String msg = "Hello World!!";
+        log.debug("END sayHello()");
+        return msg;
+    }
+}
